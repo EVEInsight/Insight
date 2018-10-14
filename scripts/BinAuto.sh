da0d98c887094c3aecb83a9ce11397043cd1c04e
Nathan <nathan@nathan-s.com>
2018-10-13 21:23:21 -0700

    Fix script.

--- scripts/BinAuto.sh
+++ scripts/BinAuto.sh
@@ -6,12 +6,11 @@ Script=$(readlink -f "$0")
 SPath=$(dirname "$Script")
 cd $SPath/..
 scripts/insight_stop.sh
-rm sqlite-latest.sqlite
 rm venv -R
 git pull || exit 1
 scripts/AutoSetup.sh || exit 1
 source venv/bin/activate
 pip3 install --upgrade pyinstaller || exit 1
-pyinstaller scripts/InsightPyInstaller.spec --clean || exit 1
+pyinstaller scripts/PyInstaller.spec --clean || exit 1
 screen -dmS eveInsight distTest/Insight || exit 1
 echo 'Insight bin version started.'
