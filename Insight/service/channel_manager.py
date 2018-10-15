e6d9be02b94e47ac7c2aec03833fc5a4a489304a
Nathan <nathan@nathan-s.com>
2018-10-14 17:58:13 -0700

    Only one request to the channel loader per channel loader can be running at a time.

--- Insight/service/channel_manager.py
+++ Insight/service/channel_manager.py
@@ -17,6 +17,7 @@ class Channel_manager(object):
         self.__channel_feed_container = {}
         self.__dm_container = {}
         self.__discord_client:discord_bot.Discord_Insight_Client = None
+        self.__id_locks = {}
         self.__delay_post = queue.Queue()
 
     async def add_delay(self, other_time):
@@ -126,19 +127,25 @@ class Channel_manager(object):
         except Exception as ex:
             print(ex)
 
-    async def get_channel_feed(self,channel_object: discord.TextChannel):
-        retry_count = 2
-        while retry_count > 0:
-            try:
-                feed = await self.__get_channel_feed(channel_object)
-                retry_count = 0
-                return feed
-            except Exception as ex:
-                print('An error occurred when loading a channel feed: {}'.format(ex))
-                traceback.print_exc()
-            retry_count -= 1
-            await asyncio.sleep(1)
-        raise InsightExc.DiscordError.ChannelLoaderError
+    async def get_channel_feed(self, channel_object: discord.TextChannel):
+        assert channel_object.id is not None
+        cLock = self.__id_locks.get(channel_object.id)
+        if not isinstance(cLock, asyncio.Lock):
+            cLock = asyncio.Lock()
+            self.__id_locks[channel_object.id] = cLock
+        async with cLock:  # race condition to prevent double loading of channels if timed correctly
+            retry_count = 4
+            while retry_count > 0:
+                try:
+                    feed = await self.__get_channel_feed(channel_object)
+                    retry_count = 0
+                    return feed
+                except Exception as ex:
+                    print('An error occurred when loading a channel feed: {}'.format(ex))
+                    traceback.print_exc()
+                retry_count -= 1
+                await asyncio.sleep(1)
+            raise InsightExc.DiscordError.ChannelLoaderError
 
     async def __get_channel_feed(self,channel_object: discord.TextChannel):
         try:
