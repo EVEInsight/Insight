289608b2b5a585273502378bce9d501cbc41776c
Nathan <nathan@nathan-s.com>
2018-11-07 20:35:47 -0800

    Add cancel option to admin console.

--- Insight/discord_bot/UnboundUtilityCommands/Admin.py
+++ Insight/discord_bot/UnboundUtilityCommands/Admin.py
@@ -8,7 +8,7 @@ class Admin(UnboundCommandBase):
 
     async def run_command(self, d_message: discord.Message, m_text: str):
         async with self.cLock:
-            options = dOpt.mapper_index(self.client, d_message)
+            options = dOpt.mapper_index_withAdditional(self.client, d_message)
             options.set_main_header("Select an Insight administrator function to execute.")
             options.add_option(dOpt.option_calls_coroutine(self.unbound.quit.command_description(), "", self.unbound.command_quit(d_message)))
             await options()
