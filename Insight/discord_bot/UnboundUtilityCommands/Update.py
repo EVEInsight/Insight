b6fa0f731a63191157d25968b76965d8d2c99622
Nathan <nathan@nathan-s.com>
2018-11-18 16:01:05 -0800

    Clarify update prompt text.

--- Insight/discord_bot/UnboundUtilityCommands/Update.py
+++ Insight/discord_bot/UnboundUtilityCommands/Update.py
@@ -1,4 +1,5 @@
 from .UnboundCommandBase import *
+import os
 
 
 class Update(UnboundCommandBase):
@@ -14,10 +15,15 @@ class Update(UnboundCommandBase):
             options = dOpt.mapper_return_yes_no(self.client, d_message)
             options.set_main_header("Are you sure you want to update Insight? This will reboot the Insight "
                                     "server application and update it.\n\nWARNING: This will NOT update or install new "
-                                    "third-party libraries used by Insight. Ensure 'requirements.txt' has not "
-                                    "changed in this new patch otherwise you must manually install the new packages"
-                                    " to your python interpreter first.\n\nNote: You may get a 'CancelledError' "
-                                    "message in Discord which you can safely ignore.")
+                                    "third-party libraries used by Insight. This update tool will NOT work for "
+                                    "versions of Insight that do not use Git and the Python interpreter. You must "
+                                    "quit Insight and start it from the command line if there were changes to any of "
+                                    "the following project files: \n\n/Insight/__main__.py\n"
+                                    "/Insight/InsightMain/InsightMain.py\n/Insight/InsightMain/InsightUpdater.py\n\n"
+                                    "\nThis tool will update the Insight Git repo located at the "
+                                    "following path to the latest commit for the set branch : \n\n'{}'\n\n "
+                                    "You may get a 'CancelledError message in Discord which you can"
+                                    " safely ignore.".format(os.getcwd()))
             resp = await options()
             if resp:
                 await self.client.update_self(d_message.author)
