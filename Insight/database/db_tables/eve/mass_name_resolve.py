4253b761f9e9fcb4b20dff7986f88a9fcb947485
Nathan <nathan@nathan-s.com>
2018-11-19 23:14:24 -0800

    Reduce high memory usage when processing large quantities of names.

--- Insight/database/db_tables/eve/mass_name_resolve.py
+++ Insight/database/db_tables/eve/mass_name_resolve.py
@@ -41,20 +41,29 @@ class name_resolve(name_only):
                 missing_object_dict[row.get_id()] = row
             id_keys = list(missing_object_dict.keys())
             id_keys = list(set(id_keys) - set(error_ids))
+            commit_pending_buffer = 0
+            completed_count = 0
             for id_list in cls.split_lists(id_keys, cls.missing_id_chunk_size()):
+                completed_count += len(id_list)
                 lg = InsightLogger.InsightLogger.get_logger('ZK.names', 'ZK.log', child=True)
-                lg.info('Processing name chunk of size {} from {} total missing names.'.format(len(id_list), len(id_keys)))
+                lg.info('Processing name chunk of size {} from {}/{} total missing names.'.format(len(id_list),
+                                                                                                  completed_count,
+                                                                                                  len(id_keys)))
                 try:
                     response = requests.post(url=cls.post_url(), headers=service_module.get_headers(lib_requests=True),
                                              json=id_list, timeout=3)
                     if response.status_code == 200:
+                        commit_pending_buffer += len(id_list)
                         for search_result in response.json():
                             selected_item = missing_object_dict.get(search_result.get('id'))
                             if selected_item is not None:
                                 selected_item.set_name(search_result.get('name'))
+                        if commit_pending_buffer >= 30000:
+                            db.commit()
+                            commit_pending_buffer = 0
                     else:
-                        lg.warning('Response {} Headers: {} Body: {} IDs: {}'.
-                                    format(response.status_code, response.headers, response.json(), str(id_list)))
+                        lg.warning('Response {} Headers: {} IDs: {}'.format(response.status_code,
+                                                                            response.headers, str(id_list)))
                         ids_404.extend(id_list)
                 except requests.exceptions.Timeout:
                     lg.info('Timeout.')
@@ -64,7 +73,8 @@ class name_resolve(name_only):
                     lg.info('Error IDs: {}'.format(str(id_list)))
                     print('Error: {} when resolving char names.'.format(ex))
                     ids_404.extend(id_list)
-            db.commit()
+            if commit_pending_buffer > 0:
+                db.commit()
         except Exception as ex:
             print(ex)
             traceback.print_exc()
