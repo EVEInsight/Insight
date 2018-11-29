822521dd61ecfeddd0a9ce9234f0a30850c28230
Nathan <nathan@nathan-s.com>
2018-11-28 17:03:10 -0800

    Add Dockerfile.

--- scripts/Docker/DockerEntry.sh
+++ scripts/Docker/DockerEntry.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # This file should only be ran inside the Insight Docker container. This script copies the config file into the volume at run time and starts the bot with any parameters passed.
-git -C /InsightDocker/Insight pull
 cp /InsightDocker/Insight/default-config.ini /app
-python3 /InsightDocker/Insight/Insight "$@"
+exec "$@"
