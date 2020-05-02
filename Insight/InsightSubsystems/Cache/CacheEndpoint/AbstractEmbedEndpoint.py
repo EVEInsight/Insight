6b3d27916deb7e0652990dc423cf2689ddbdf390
Nathan-LS <nathan@nathan-s.com>
2020-05-02 01:10:23 -0700

    Rewrite AbstractEmbedEndpoint

--- Insight/InsightSubsystems/Cache/CacheEndpoint/AbstractEmbedEndpoint.py
+++ Insight/InsightSubsystems/Cache/CacheEndpoint/AbstractEmbedEndpoint.py
@@ -7,31 +7,11 @@ from functools import partial
 
 
 class AbstractEmbedEndpoint(AbstractEndpoint):
-    def default_color(self) -> int:
-        return 659493
+    def default_color(self) -> discord.Color:
+        return discord.Color(659493)
 
     def make_embed(self, d: dict) -> discord.Embed:
-        e = discord.Embed()
-        e.color = Helpers.get_nested_value(d, 659493, "color")
-        e.timestamp = datetime.utcfromtimestamp(Helpers.get_nested_value(d, datetime.utcnow(), "timestamp"))
-        e.set_author(name=Helpers.get_nested_value(d, EmptyEmbed, "author", "name"),
-                     url=Helpers.get_nested_value(d, EmptyEmbed, "author", "url"),
-                     icon_url=Helpers.get_nested_value(d, EmptyEmbed, "author", "icon_url"))
-        for f in Helpers.get_nested_value(d, [], "fields"):
-            e.add_field(name=Helpers.get_nested_value(f, "-", "name"),
-                        value=Helpers.get_nested_value(f, "-", "value"),
-                        inline=Helpers.get_nested_value(f, "-", "inline"))
-        image = Helpers.get_nested_value(d, None, "image")
-        if image:
-            e.set_image(url=image)
-        desc = Helpers.get_nested_value(d, None, "description")
-        if desc:
-            e.description = desc
-        thumbnail = Helpers.get_nested_value(d, None, "thumbnail")
-        if thumbnail:
-            e.set_thumbnail(url=thumbnail)
-        e.set_footer(text=Helpers.get_nested_value(d, EmptyEmbed, "footer", "text"),
-                     icon_url=Helpers.get_nested_value(d, EmptyEmbed, "footer", "icon_url"))
+        e = discord.Embed.from_dict(d)
         return e
 
     async def get(self, **kwargs) -> discord.Embed:
