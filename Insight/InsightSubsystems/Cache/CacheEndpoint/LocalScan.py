e71b01a5f8391119afc5496537bd43e14f4af857
Nathan-LS <nathan@nathan-s.com>
2020-05-16 00:46:43 -0700

    Rewrite local scan endpoint.

--- Insight/InsightSubsystems/Cache/CacheEndpoint/LocalScan.py
+++ Insight/InsightSubsystems/Cache/CacheEndpoint/LocalScan.py
@@ -1,81 +1,11 @@
 from InsightSubsystems.Cache.CacheEndpoint.AbstractEndpoint import AbstractEndpoint
 from InsightUtilities.StaticHelpers import *
 from datetime import datetime
-from statistics import mean
 from dateutil.parser import parse as dateTimeParser
 from operator import itemgetter
-
-
-class BucketManagerEntity(object):
-    def __init__(self, name):
-        self.name = name
-        self.ships = {}
-        self.ship_delays = {}
-        self.locations = {}
-        self.systems = {}
-        self.mails = {}
-        self.total = 0
-        self.alive = 0
-        self.dead = 0
-
-    def add_ship(self, ship_name: str, seconds_ago: float, system_data: dict, location_data: dict, km_data: dict,
-                 is_attacker: bool):
-        ship_counts = self.ships.setdefault(ship_name, {"ship": ship_name, "alive": 0, "lost": 0, "total": 0, "avgSeconds": 0})
-        ship_delays: list = self.ship_delays.setdefault(ship_name, [])
-        ship_delays.append(seconds_ago)
-        self.total += 1
-        if is_attacker:
-            ship_counts["alive"] += 1
-            self.alive += 1
-        else:
-            if ship_name != "UNKNOWN":
-                ship_counts["lost"] += 1
-                self.dead += 1
-            else:
-                ship_counts["alive"] += 1
-                self.alive += 1
-        ship_counts["total"] += 1
-        if location_data:
-            loc_id = Helpers.get_nested_value(location_data, 0, "location_id")
-            locations = self.locations.setdefault(loc_id, {"location": location_data, "total": 0})
-            locations["total"] += 1
-        if system_data:
-            system_id = Helpers.get_nested_value(system_data, 0, "system_id")
-            s = self.systems.setdefault(system_id, {"system": system_data, "total": 0})
-            s["total"] += 1
-        if km_data:
-            km_id = Helpers.get_nested_value(system_data, 0, "killmail_id")
-            k = self.systems.setdefault(km_id, {"km": km_data, "total": 0})
-            k["total"] += 1
-
-    def get_sorted_ships(self) -> list:
-        tmp_ship_list = []
-        for k, v in self.ships.items():
-            avg_delay = mean(self.ship_delays[k]) if len(self.ship_delays[k]) else 0
-            v["avgSeconds"] = avg_delay
-            tmp_ship_list.append(v)
-        return sorted(tmp_ship_list, key=itemgetter("total"), reverse=True)
-
-    def get_top_dict(self, counttracking_dict):
-        if len(counttracking_dict) == 0:
-            return None
-        else:
-            d = max(counttracking_dict.values(), key=itemgetter("total"))
-            d["ratio"] = d["total"] / self.total
-            return d
-
-    def to_dict(self) -> dict:
-        return_dict = {
-            "name": self.name,
-            "ships": self.get_sorted_ships(),
-            "systemHighestRatio": self.get_top_dict(self.systems),
-            "locationHighestRatio": self.get_top_dict(self.locations),
-            "kmHighestRatio": self.get_top_dict(self.mails),
-            "alive": self.alive,
-            "dead": self.dead,
-            "total": self.total
-        }
-        return return_dict
+from InsightSubsystems.Cache.CacheEndpoint.EndpointUtils.TrackingBucket import TrackingBucket, TrackingBucketDual
+from InsightSubsystems.Cache.CacheEndpoint.EndpointUtils.TrackingBucketEntity import TrackingBucketEntity
+from collections import OrderedDict
 
 
 class LocalScan(AbstractEndpoint):
@@ -102,51 +32,87 @@ class LocalScan(AbstractEndpoint):
 
     @classmethod
     def _do_endpoint_logic_sync(cls, data_dict: dict, char_names: frozenset, seconds_ago_threshold: int):
+        known_ships = Helpers.get_nested_value(data_dict, [], "data", "known")
         bucket_alliance = {}
         bucket_corp = {}
-        all_items = []
-        known_ships = Helpers.get_nested_value(data_dict, [], "data", "known")
+        bucket_ships = TrackingBucketDual("shipID", "ship", "characters", value_as_list=True)
+        bucket_systems = TrackingBucketDual("systemID", "system", "characters", value_as_list=True)
+        bucket_locations = TrackingBucketDual("locationID", "location", "characters", value_as_list=True)
+        bucket_mails = TrackingBucketDual("kmID", "km", "characters", value_as_list=True)
+        all_characters = {}
         for e in known_ships:
+            is_unknown = False
             seconds_ago = (datetime.utcnow() - dateTimeParser(Helpers.get_nested_value(e, datetime.utcnow(), "time"))).total_seconds()
             if seconds_ago > seconds_ago_threshold:
                 if (not Helpers.get_nested_value(e, False, "isSuperTitan")
                         and not Helpers.get_nested_value(e, False, "isRegularCap")):
-                    ship_name = "UNKNOWN"
-                else:
-                    ship_name = Helpers.get_nested_value(e, "UNKNOWN", "ship", "type_name")
-            else:
-                ship_name = Helpers.get_nested_value(e, "UNKNOWN", "ship", "type_name")
+                    is_unknown = True
+            if not Helpers.get_nested_value(e, None, "ship", "type_name"):
+                is_unknown = True
             is_attacker = Helpers.get_nested_value(e, False, "attacker")
+            char_id = Helpers.get_nested_value(e, None, "character", "character_id")
+            ship_data = Helpers.get_nested_value(e, {}, "ship")
+            ship_id = Helpers.get_nested_value(ship_data, 0, "type_id")
             system_data = Helpers.get_nested_value(e, {}, "system")
+            system_id = Helpers.get_nested_value(system_data, 0, "system_id")
             location_data = Helpers.get_nested_value(e, {}, "location")
+            location_id = Helpers.get_nested_value(location_data, 0, "location_id")
             km_data = Helpers.get_nested_value(e, {}, "km", "package", "killmail")
-            corp_name = Helpers.get_nested_value(e, None, "corporation", "corporation_name")
-            alliance_name = Helpers.get_nested_value(e, None, "alliance", "alliance_name")
-            if alliance_name:
-                alliance_bucket = bucket_alliance.setdefault(alliance_name, BucketManagerEntity(alliance_name))
-                alliance_bucket.add_ship(ship_name, seconds_ago, system_data, location_data, km_data, is_attacker)
-            elif corp_name:
-                corp_bucket = bucket_corp.setdefault(corp_name, BucketManagerEntity(corp_name))
-                corp_bucket.add_ship(ship_name, seconds_ago, system_data, location_data, km_data, is_attacker)
+            km_id = Helpers.get_nested_value(km_data, 0, "killmail_id")
+            corp_data = Helpers.get_nested_value(e, None, "corporation")
+            corp_id = Helpers.get_nested_value(corp_data, 0, "corporation_id")
+            corp_name = Helpers.get_nested_value(corp_data, None, "corporation_name")
+            alliance_data = Helpers.get_nested_value(e, None, "alliance")
+            alliance_id = Helpers.get_nested_value(alliance_data, 0, "alliance_id")
+            alliance_name = Helpers.get_nested_value(alliance_data, None, "alliance_name")
+            if alliance_id:
+                alliance_bucket = bucket_alliance.setdefault(
+                    alliance_id, TrackingBucketEntity(alliance_id, alliance_name, True, alliance_data))
+                alliance_bucket.add_ship(seconds_ago, ship_id, char_id, system_id, location_id,
+                                         km_id, is_attacker, is_unknown)
+            elif corp_id:
+                corp_bucket = bucket_corp.setdefault(
+                    corp_id, TrackingBucketEntity(corp_id, corp_name, False, corp_data))
+                corp_bucket.add_ship(seconds_ago, ship_id, char_id, system_id, location_id,
+                                     km_id, is_attacker, is_unknown)
             else:
                 pass
-            all_d = {
-                "character": Helpers.get_nested_value(e, None, "character"),
-                "corporation": Helpers.get_nested_value(e, None, "corporation"),
-                "alliance": Helpers.get_nested_value(e, None, "alliance"),
-                "shipName": ship_name,
-                "attacker": is_attacker,
-                "seconds": seconds_ago,
-                "system": system_data,
-                "location": location_data
-            }
-            all_items.append(all_d)
-        alliances_buckets_sorted = [v.to_dict() for v in bucket_alliance.values()]
-        corporation_buckets_sorted = [v.to_dict() for v in bucket_corp.values()]
+            if char_id:
+                bucket_mails.add_item(km_id, km_data, char_id)
+                bucket_systems.add_item(system_id, system_data, char_id)
+                bucket_locations.add_item(location_id, location_data, char_id)
+                if is_unknown:
+                    bucket_ships.add_item(None, None, char_id)
+                else:
+                    bucket_ships.add_item(ship_id, ship_data, char_id)
+                all_d = {
+                    "characterID": char_id,
+                    "corporationID": corp_id,
+                    "allianceID": alliance_id,
+                    "shipID": ship_id,
+                    "attacker": is_attacker,
+                    "unknown": is_unknown,
+                    "seconds": seconds_ago,
+                    "systemID": system_id,
+                    "locationID": location_id
+                }
+                all_characters[char_id] = all_d
+        alliances_buckets_sorted = sorted([v.to_dict() for v in bucket_alliance.values()], key=itemgetter("total"), reverse=True)
+        corporation_buckets_sorted = sorted([v.to_dict() for v in bucket_corp.values()], key=itemgetter("total"), reverse=True)
+        ordered_alliances = OrderedDict((d["id"], d) for d in alliances_buckets_sorted)
+        ordered_corps = OrderedDict((d["id"], d) for d in corporation_buckets_sorted)
         return_dict = {
-            "alliances": alliances_buckets_sorted,
-            "corporations": corporation_buckets_sorted,
-            "all": all_items,
+            "alliances": ordered_alliances,
+            "corporations": ordered_corps,
+            "characters": all_characters,
+            "shipHighestRatio": bucket_ships.get_top_dict(),
+            "ships": bucket_ships.get_sorted_dict(),
+            "systemHighestRatio": bucket_systems.get_top_dict(),
+            "systems": bucket_systems.get_sorted_dict(),
+            "locationHighestRatio": bucket_locations.get_top_dict(),
+            "locations": bucket_locations.get_sorted_dict(),
+            "kmHighestRatio": bucket_mails.get_top_dict(),
+            "kms": bucket_mails.get_sorted_dict(),
             "queriedNames": list(char_names),
             "totalQueried": len(char_names),
             "totalUnknownNames": len(char_names) - len(known_ships)
