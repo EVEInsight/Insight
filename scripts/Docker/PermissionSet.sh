8e75250a620e4816ac3c9139e5254a316a83af0f
Nathan-LS <nathan@nathan-s.com>
2020-07-16 23:44:40 -0700

    Fix entry script.

--- scripts/Docker/PermissionSet.sh
+++ scripts/Docker/PermissionSet.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo chown -R insight:insight /app || exit 1
+sudo chmod 750 /app || exit 1
 cp -n /InsightDocker/Insight/default-config.ini /app || exit 1
 cp -n /InsightDocker/Insight/default-config.ini /app/config.ini || exit 1
 cp -n /InsightDocker/Insight/README.md /app || exit 1
